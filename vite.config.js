e5e19a800fda5d726234bc50dd658a8285cef317
Nico <nicolas.gabriel.1314@gmail.com>
2025-09-26 21:00:26 -0300

    Segundo commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Nico2530.github.io/TP3',
+  //base: 'https://Nico2530.github.io/TP3', (Segun el video de youtube)
+  base: process.env.NODE_ENV === 'production' //(Segun Cloude)
+    ? '/TP3/'
+    : '/',
 })
