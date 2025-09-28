6cce33eaa8302c192d80470fb0a0144d13555ff3
Nico <nicolas.gabriel.1314@gmail.com>
2025-09-27 23:52:09 -0300

    Reorganizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router'
+import { Link, Route, Routes } from 'react-router'
 import PageFile from './pages/PageFile'
 import PageEmail from './pages/PageEmail'
 import './App.css'
@@ -7,22 +7,21 @@ function App() {
 
   return (
     <>
-      <BrowserRouter basename='/TP3/'>
-
+      <header>
         <h1>TP3</h1>
 
         <nav>
           <Link to="/" className='nav-link'>Email</Link>
           <Link to="/File" className='nav-link'>File</Link>
         </nav>
+      </header>
 
+      <main>
         <Routes>
           <Route path="/" element={<PageEmail />} />
           <Route path="/File" element={<PageFile />} />
         </Routes>
-
-      </BrowserRouter>
-
+      </main>
     </>
   )
 }
